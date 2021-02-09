580600f5c5e8715173cb3046c4d2fb07170e006f
patman007 <patrickmason007@gmail.com>
2021-02-09 14:31:02 -0600

    Tic Tac Toe 2

--- Tic_Tac_Toe/main.py
+++ Tic_Tac_Toe/main.py
@@ -6,8 +6,10 @@ board = ["-", "-", "-",
 
 #If game is still going
 game_still_going = True
+
 #Who won? or Tie?
 winner = None
+
 #Whos turn is it
 current_player = "X"
 
@@ -19,6 +21,7 @@ def display_board():
 
 #Play Game Function
 def play_game():
+
     #Display initial board
     display_board()
 
@@ -36,7 +39,7 @@ def play_game():
     #IF STATEMENT
     #The game has ended
     if winner == "X" or winner == "O":
-        print(winner + "won.")
+        print(winner + " won.")
     elif winner == None:
         print("Tie.")
 
@@ -46,7 +49,7 @@ def handle_turn(player):
     #Make sure the position starts at 1 instead of 0
     position = int(position) - 1
 
-    board[position] = "X"
+    board[position] = player
     display_board()
 
 #Check if Game Over Function
@@ -56,6 +59,10 @@ def check_if_game_over():
 
 #Check if Win Function
 def check_for_winner():
+
+    #Set up Global variables
+    global winner
+
     #Check rows
     row_winner = check_rows()
 
@@ -66,30 +73,91 @@ def check_for_winner():
     diagonal_winner = check_diagonals()
     if row_winner:
         #there was a win
-        winner = row_winner()
+        winner = row_winner
     elif column_winner:
         #there was a win
-        winner = column_winner()
+        winner = column_winner
     elif diagonal_winner:
-        winner = diagonal_winner()
+        #there was a win
+        winner = diagonal_winner
     else:
         winner = None
     return
 
 def check_rows():
-    return
+    #Set up global variables
+    global game_still_going
+    #Check if any of rows have the same value without a dash
+    row_1 = board[0] == board[1] == board[2] !="-"
+    row_2 = board[3] == board[4] == board[5] !="-"
+    row_3 = board[6] == board[7] == board[8] !="-"
+
+    #If any row does have a match, flag that there is a win
+    if row_1 or row_2 or row_3:
+        game_still_going = False
+        #Return the winner (X or O)
+        #We could use any element number besides listed here
+        if row_1:
+            return board[0]
+        elif row_2:
+            return_board[3]
+        elif row_3:
+            return board[6]
+        return
 
 def check_columns():
-    return
+        #Set up global variables
+    global game_still_going
+    #Check if any of rows have the same value without a dash
+    column_1 = board[0] == board[3] == board[6] !="-"
+    column_2 = board[1] == board[4] == board[7] !="-"
+    column_3 = board[2] == board[5] == board[8] !="-"
+
+    #If any row does have a match, flag that there is a win
+    if column_1 or column_2 or column_3:
+        game_still_going = False
+        #Return the winner (X or O)
+        #We could use any element number besides listed here
+        if column_1:
+            return board[0]
+        elif column_2:
+            return board[1]
+        elif column_3:
+            return board[2]
+        return
 
 def check_diagonals():
-    return
+            #Set up global variables
+    global game_still_going
+    #Check if any of rows have the same value without a dash
+    diagonal_1 = board[0] == board[4] == board[8] !="-"
+    diagonal_2 = board[6] == board[4] == board[2] !="-"
+
+    #If any row does have a match, flag that there is a win
+    if diagonal_1 or diagonal_2:
+        game_still_going = False
+        #Return the winner (X or O)
+        #We could use any element number besides listed here
+        if diagonal_1:
+            return board[0]
+        elif diagonal_2:
+            return board[6]
+        return
 
 #Check if Tie Function
 def check_if_tie():
+    if "-" not in board:
+        game_still_going = False
     return
 
 def flip_player():
+    #Global variable
+    global current_player
+    #If the current player was X, then change it to O
+    if current_player == "X":
+        current_player = "O"
+    elif current_player =="O":
+        current_player = "X"
     return
 
 #Play Game
